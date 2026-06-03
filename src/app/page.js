b2cb4bcdb399de3a54b42fca355cf7ea11559c6d
Parvez Mahmud Lalin <parvezmahmudlalin@gmail.com>
2026-06-03 12:15:50 -0700

    added marquee section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/Banner";
-import Image from "next/image";
+import MarqueeSection from "@/components/MarqueeSection";
+
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
     <Banner />
+    <MarqueeSection />
     </div>
   );
 }
---
+++ src/components/MarqueeSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Marquee from "react-fast-marquee";
+const MarqueeSection = () => {
+    return (
+        <Marquee>
+  I can be a React component, multiple React components, or just some text.
+</Marquee>
+    );
+};
+
+export default MarqueeSection;

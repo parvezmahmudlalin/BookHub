8ce12e7d49b0b0c9469751754273e51d420e330d
Parvez Mahmud Lalin <parvezmahmudlalin@gmail.com>
2026-06-03 12:55:22 -0700

    fix marquee section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Banner from "@/components/Banner";
 import MarqueeSection from "@/components/MarqueeSection";
+import Marquee from "react-fast-marquee";
 
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
     <Banner />
-    <MarqueeSection />
+    <Marquee><MarqueeSection /> </Marquee>
+    
     </div>
   );
 }
--- src/components/MarqueeSection.jsx
+++ src/components/MarqueeSection.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
-import Marquee from "react-fast-marquee";
-const MarqueeSection = () => {
-    return (
-        <Marquee>
-  I can be a React component, multiple React components, or just some text.
-</Marquee>
-    );
+const MarqueeSection = async () => {
+  const res = await fetch("http://localhost:3000/data.json", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch books");
+  }
+
+  const books = await res.json();
+  const latestBooks = books.slice(0, 6);
+
+  const bookNames = latestBooks.map((book) => book.title).join(" | ");
+
+  return (
+    <div className="bg-gray-200 dark:bg-gray-900 py-3 overflow-hidden my-6 rounded-lg flex items-center">
+      <div className="px-4  whitespace-nowrap font-bold text-pink-500 text-lg font-extrabold">
+        New Arrivals:
+      </div>
+
+      <div className="flex-1 overflow-hidden">
+        <div className="whitespace-nowrap animate-marquee px-4 text-lg text-gray-700 dark:text-gray-300 font-bold">
+          {bookNames} |
+          <span className="text-blue-600">Special Discount on Memberships</span>
+          |
+          <span className="text-pink-950">
+            Join BookHub & Start Reading Today
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MarqueeSection;
